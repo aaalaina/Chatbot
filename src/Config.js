80bc9852c971278a354b9e09427bf23c8c469d84
Alaina H <97alaina@gmail.com>
2022-08-01 14:56:19 -0400

    Added starter codes

---
+++ src/Config.js
@@ -0,0 +1,10 @@
+//import React from "react";
+
+// Config starter code
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  initialMessages: [createChatBotMessage(`Hello world`)]
+}
+
+export default config;
